b0022092a9e4343063660135ba101fdefe10f8d0
Chetan Chavan <111604779+1165CHETAN@users.noreply.github.com>
2023-12-02 10:33:49 +0530

    modified name in title

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WeaveHub",
+  title: "WeaveHub | Chetan Chavan",
   description: "A Next.js 13 Meta Threads application",
 };
 
